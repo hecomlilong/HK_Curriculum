cfa71ba6ca9b2d436bff6e8d80e01ea3a72c6ba9
BruceLi <lilong01@hecom.cn>
2017-07-08 13:38:47 +0800

    first commit

---
+++ application/index/model/users.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\model;
+
+class Users extends \think\Model
+{
+	/**
+     * 架构函数
+     * @access public
+     * @param array|object $data 数据
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
